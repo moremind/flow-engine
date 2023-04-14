efb119a05f68706b7ff6a7e86a4b89e7fb6b35b7
moremind <hefengen@apache.org>
2023-04-15 00:31:18 +0800

    init project

---
+++ flow-bootstrap/src/main/java/cn/moremind/FlowBootstrapApplication.java
@@ -0,0 +1,12 @@
+package cn.moremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowBootstrapApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(FlowBootstrapApplication.class, args);
+    }
+}
---
+++ flow-common/src/main/java/cn/moremind/Main.java
@@ -0,0 +1,7 @@
+package cn.moremind;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
